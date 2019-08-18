0d8bd34c5010566ec9792e4f2309e2c7fce24165
tommason14 <tom.mason14@gmail.com>
2019-08-18 16:26:32 +1000

    now need to do the same with bash aliases- specific to host

--- make_shortcuts/make_shortcuts.sh
+++ make_shortcuts/make_shortcuts.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+declare -A conversion
+conversion["mgs"]="magnus"
+conversion["rjn"]="raijin"
+conversion["mon"]="monarch"
+conversion["mas"]="m3"
+conversion["stm"]="stampede"
+conversion["uni"]="MU00151959X"
+conversion["home"]="macbook"
+
 rm_comments(){
   sed '/^#/d'
 }
@@ -18,55 +27,53 @@ rm_supercomps(){
 
 clean(){
   rm_comments |\
- delete_blanks |\
+  delete_blanks |\
   rm_supercomps
 }
 
-declare -A conversion
-conversion["mgs"]="magnus"
-conversion["rjn"]="raijin"
-conversion["mon"]="monarch"
-conversion["mas"]="m3"
-conversion["stm"]="stampede"
-conversion["uni"]="MU00151959X"
-conversion["home"]="macbook"
 
 make_supercomp_configs(){
-scheduler=$1 # slurm or pbs (or local)
-supercomps=${@:2} 
-for f in $supercomps
-do
-  long_hostname=${conversion[$f]}
-  cp ../lf/lfrc.base.$scheduler lf.automated.$long_hostname
-  printf "\n# files {{{2\n\n" >> lf.automated.$long_hostname
-  cat files | sort | clean | sed -e 's/^/map o/' -e 's/:/ $vim/' >> lf.automated.$long_hostname
-  printf "\n# movement {{{2\n\n" >> lf.automated.$long_hostname
-  cat folders |\
-  sort |\
-  delete_blanks |\
-  rm_comments |\
-  sed -n "/$f-/p" |\
-  sed -e "s/^$f-/map g/" -e 's/:/ $cd/' >> lf.automated.$long_hostname
-done
+  scheduler=$1 # slurm or pbs
+  supercomps=${@:2} 
+  for f in $supercomps
+  do
+    long_hostname=${conversion[$f]}
+    cp ../lf/lfrc.base.$scheduler ../lf/lfrc.$long_hostname
+    printf "\n# Files {{{2\n\n" >> ../lf/lfrc.$long_hostname
+    cat files | sort | clean | sed -e 's/^/map o/' -e 's/:/ $vim/' >> ../lf/lfrc.$long_hostname
+    printf "\n# Movement {{{2\n\n" >> ../lf/lfrc.$long_hostname
+    cat folders | sort | delete_blanks | rm_comments | sed -n "/$f-/p" |\
+    sed -e "s/^$f-/map g/" -e 's/:/ $cd/' >> ../lf/lfrc.$long_hostname
+  done
 }
 
-for f in ${locals[@]}
-do
-  cp ../lf/lfrc.base lf.automated.$f
-  printf "\n# files {{{2\n\n" >> lf.automated.$f
-  cat files | sort | clean | sed -e 's/^/map o/' -e 's/:/ $vim/' >> lf.automated.$f 
-  printf "\n# movement {{{2\n\n" >> lf.automated.$f
-  cat folders | sort | clean | sed 's/^/map g/;s/:/ $cd/' >> lf.automated.$f
-done 
+make_local_configs(){
+  for f in $@
+  do
+    long_hostname=${conversion[$f]}
+    cp ../lf/lfrc.base.local ../lf/lfrc.$long_hostname
+    printf "\n# Files {{{2\n\n" >> ../lf/lfrc.$long_hostname
+    cat files | sort | clean  | sed -e 's/^/map o/' -e 's/:/ $vim/' >> ../lf/lfrc.$long_hostname
+    printf "\n# Movement {{{2\n\n" >> ../lf/lfrc.$long_hostname
+    cat folders | sort | clean | sed 's/^/map g/;s/:/ $cd/' >> ../lf/lfrc.$long_hostname
+  done 
+}
 
-locals="MU00151959X macbook"
-slurm="mgs mas mon stm"
-pbs="rjn"
-make_configs 'local' ${locals[@]}
-make_configs 'slurm' ${slurm[@]}
-make_supercomp_configs 'pbs' ${pbs[@]}
+make_bash_aliases(){
+  printf "\n# Files {{{2\n\n" > bash_aliases
+  cat files | sort | clean | sed -e 's/^/alias o/' -e "s/:/='vim/" -e "s/$/'/" >> bash_aliases
+  printf "\n# Movement {{{2\n\n" >> bash_aliases
+  cat folders | sort | clean | sed -e 's/^/alias g/' -e "s/:/='cd/" -e "s/$/'/" >> bash_aliases
+}
 
-# everywhere
+main(){
+  locals="uni home"
+  slurm="mgs mas mon stm"
+  pbs="rjn"
+  make_local_configs ${locals[@]}
+  make_supercomp_configs 'slurm' ${slurm[@]}
+  make_supercomp_configs 'pbs' ${pbs[@]}
+  make_bash_aliases
+}
 
-cat files | sort | clean | sed -e 's/^/alias o/' -e "s/:/='vim/" -e "s/$/'/" > bash_aliases
-cat folders | sort | clean | sed -e 's/^/alias g/' -e "s/:/='cd/" -e "s/$/'/" >> bash_aliases
+main
