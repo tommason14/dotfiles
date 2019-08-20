7cdd9c9ec0dd7961379d8f5fce271ad842346837
Tom Mason <tommason14@gmail.com>
2019-08-20 20:14:42 +1000

    added wallpaper function

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -12,7 +12,7 @@ then
       *.rar) unrar l "$1";;
       *.7z) 7z l "$1";;
       *.pdf) pdftotext "$1" -;;
-      *.jpg) feh --borderless "$1";;
+      # *.jpg) feh --borderless "$1";;
       # *.jpg) montage -tile 7x1 -label %f -background black -fill white "$@" gif:- | convert - -colors 16 sixel:-; ;;
       *) highlight -O ansi "$1" || cat "$1";;
   esac
