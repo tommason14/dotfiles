8908467a2a2991f7dbeb779fbd04b377ca2a8da6
Tom Mason <tommason14@gmail.com>
2019-09-12 22:04:30 +1000

    added brew.sh

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# bash utils
+brew install entr
+brew install gnuplot
+brew install imagemagick
+brew install lf
+brew install pandoc
+brew install pandoc-citeproc
+brew install pandoc-crossref
+brew install python
+brew install r
+brew install ranger
+brew install tree
+brew install tmux
+
+
+# gui programs
+brew cask install google-drive-file-stream
+
+brew cask install spectacle
+curl https://gist.githubusercontent.com/tommason14/324cb37dd7803f3b06b39f53d38301f6/raw/spectacle.json
+-o ~/Library/Application\ Support/Spectacle/Shortcuts.json
+
+brew cask install visual-studio-code 
+brew cask install zotero
