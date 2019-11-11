58645fa271045f2fa697e390f861ef23a6939e40
Tom Mason <tommason14@gmail.com>
2019-11-12 10:45:07 +1100

    wal setting wallpaper issues, so pass to macOS instead

--- lf/wallpaper.sh
+++ lf/wallpaper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if [[ $USER == "tommason" ]]
-then
-  wal -i "$1"
-else
-  wal -ni "$1"
-  /usr/bin/osascript << END
-  tell application "Finder" to set desktop picture to POSIX file "$1"
+# if [[ $USER == "tommason" ]]
+# then
+  # wal -i "$1"
+# else
+wal -ni "$1"
+/usr/bin/osascript << END
+tell application "Finder" to set desktop picture to POSIX file "$1"
 END
-fi
+# fi
